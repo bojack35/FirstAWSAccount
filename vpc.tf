0bd5e4bc18b9bfa7006528f835ab466b4863fd01
bojack35 <curjackson223@gmail.com>
2026-02-22 16:24:05 -0500

    update vpc cidr

--- vpc.tf
+++ vpc.tf
@@ -32,7 +32,7 @@ resource "aws_subnet" "this" {
   count = 4
 
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = cidrsubnet(aws_vpc.this.cidr_block, 16, count.index)
+  cidr_block              = cidrsubnet(aws_vpc.this.cidr_block, 8, count.index)
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = false
 
